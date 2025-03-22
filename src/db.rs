6c72ecb5dd6d6586f52226159c5c11a0764aefa7
Skylar Bleed <sbleed@proton.me>
2025-03-22 15:25:51 -0500

    various changes

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,9 @@
-use crate::{Res, err_fmt};
-use std::mem::transmute;
+use crate::{err_fmt, Res};
+use std::{
+    collections::HashMap,
+    mem::transmute,
+    ops::{Index, IndexMut},
+};
 
 macro_rules! push {
     ($v:expr => [$($x:expr),*]) => {{
@@ -39,54 +43,137 @@ pub enum Type {
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Row<'a>(pub &'a [Obj]);
+pub struct Row(pub usize, pub usize);
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Table<'a> {
+pub struct Table {
     head: Vec<Type>,
-    body: Vec<&'a Row<'a>>,
+    body: Vec<usize>,
+}
+
+impl Table {
+    pub fn iter<'a>(&'a self, db: &'a DB) -> RowIter<'a> {
+        RowIter {
+            i: 0,
+            len: self.head.len(),
+            db,
+            body: self.body.iter(),
+        }
+    }
+}
+
+pub struct RowIter<'a> {
+    pub i: usize,
+    pub len: usize,
+    pub db: &'a DB,
+    pub body: std::slice::Iter<'a, usize>,
+}
+
+impl<'a> Iterator for RowIter<'a> {
+    type Item = &'a [Obj];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(Row(i, e)) = self.body.next().map(|x| self.db.rows[*x]) {
+            Some(&self.db.objs[i..e])
+        } else {
+            None
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct DB<'a> {
-    pub strs: Vec<String>,
-    pub objs: Vec<Obj>,
-    pub rows: Vec<Row<'a>>,
-    pub tables: Vec<Table<'a>>,
+pub struct Map<T>(pub usize, pub HashMap<usize, T>);
+
+impl<T> Map<T> {
+    pub fn new() -> Self {
+        Self(0, HashMap::new())
+    }
+
+    pub fn push(&mut self, x: T) {
+        self.1.insert(self.0, x);
+        self.0 += 1;
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.1.len()
+    }
+
+    #[inline]
+    pub fn iter(&self) -> std::collections::hash_map::Iter<'_, usize, T> {
+        self.1.iter()
+    }
 }
 
-impl<'a> DB<'a> {
+impl<T> Index<usize> for Map<T> {
+    type Output = T;
+
+    fn index(&self, x: usize) -> &Self::Output {
+        &self.1[&x]
+    }
+}
+
+impl<T> IndexMut<usize> for Map<T> {
+    fn index_mut(&mut self, x: usize) -> &mut Self::Output {
+        self.1.get_mut(&x).expect("key not found")
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DB {
+    pub strs: Map<String>,
+    pub objs: Vec<Obj>,
+    pub rows: Map<Row>,
+    pub tables: Map<Table>,
+}
+
+impl DB {
     pub fn new() -> Self {
         Self {
-            strs: Vec::new(),
+            strs: Map::new(),
             objs: Vec::new(),
-            rows: Vec::new(),
-            tables: Vec::new(),
+            rows: Map::new(),
+            tables: Map::new(),
         }
     }
 
-    pub fn dump(&'a self) -> String {
-        self.tables.iter()
-            .enumerate()
-            .map(|(i, x)| format!(
+    pub fn dump(&self) -> String {
+        self.tables
+            .iter()
+            .map(|(i, x)| {
+                format!(
                     "table {i:3}:\n{}\n{}",
-                    x.head.iter().map(|x| format!("{x:?}\n")).collect::<String>(),
-                    x.body.iter().map(|x| format!("{x:?}\n")).collect::<String>()
-            ))
+                    x.head
+                        .iter()
+                        .map(|x| format!("{:12} ", format!("{x:?}")))
+                        .collect::<String>(),
+                    x.iter(self)
+                        .map(|x| x
+                            .iter()
+                            .map(|x| format!("{:12} ", format!("{x:?}")))
+                            .collect::<String>())
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                )
+            })
             .collect::<Vec<String>>()
             .join("\n")
     }
 
-    pub fn insert_row(&'a mut self, t: usize, x: &[Obj]) -> Res<usize> {
-        let t = &mut self.tables[t];
-        if t.head.len() != x.len() {
-            return err_fmt!("mismatched length in insert_row(): {} != {}", t.head.len(), x.len());
+    pub fn insert_row(&mut self, t: usize, x: &[Obj]) -> Res<usize> {
+        let tab = &mut self.tables[t];
+        if tab.head.len() != x.len() {
+            return err_fmt!(
+                "mismatched length in insert_row(): {} != {}",
+                tab.head.len(),
+                x.len()
+            );
         }
 
         let (i, e) = push_each!(self.objs => x);
-        let r = push!(self.rows => [Row(&self.objs[i..e])]);
+        let r = push!(self.rows => [Row(i, e)]);
 
-        t.body.push(&self.rows[r]);
+        tab.body.push(r);
         Ok(r)
     }
 
@@ -106,6 +193,8 @@ mod test {
     fn construct() {
         let mut db = DB::new();
         let t = db.new_table(vec![Type::I64, Type::I64]);
-        let r = db.insert_row(t, &[Obj(0), Obj(1)]).unwrap();
+        db.insert_row(t, &[Obj(0), Obj(1)]).unwrap();
+        db.insert_row(t, &[Obj(2), Obj(3)]).unwrap();
+        println!("{db:#?}\n{}", db.dump());
     }
 }
